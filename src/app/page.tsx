2f2d0464e991287df503cabc7627ccc8f01f1ae4
Hrudayesh <hrudayesh22@gmail.com>
2024-07-21 19:24:40 +0530

    chore: Initiated Next-auth for user authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <h1 className="text-3xl font-bold underline text-red-600">
-      Hello world!
-    </h1>
-  )
+  return <h1 className='text-4xl'>Home</h1>;
 }
